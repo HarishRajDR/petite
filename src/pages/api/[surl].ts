670e00bf9bdc258ddd65c810e3e98e472e3315a9
HarishRajDR <47946196+HarishRajDR@users.noreply.github.com>
2022-09-03 12:15:16 +0530

    Working!

--- src/pages/api/[surl].ts
+++ src/pages/api/[surl].ts
@@ -2,7 +2,33 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../db/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const query = req.query;
+  const surl = req.query["surl"];
 
-  console.log(query);
+  console.log(surl);
+
+  if (!surl || typeof surl !== "string") {
+    res.statusCode = 404;
+
+    res.send(JSON.stringify({ message: "Please use with short url phrase" }));
+
+    return;
+  }
+
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      surl: {
+        equals: surl,
+      },
+    },
+  });
+
+  if (!data) {
+    res.statusCode = 404;
+
+    res.send(JSON.stringify({ message: "URL Does not exist" }));
+
+    return;
+  }
+
+  return res.redirect(data.url);
 };
